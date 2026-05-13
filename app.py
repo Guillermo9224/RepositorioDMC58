8cf43ed21b2886fbb22afbef9f8b264667612fbd
Guillermo9224 <guillermo920624@gmail.com>
2026-05-12 19:48:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,5 @@ st.title ("Mi primera aplicacion en python")
 st.sidebar.title ("Parametros")
 
 st.write("Elaborado por : Guillermo Carrasco")
+
+sesion = st.selectbox("Seleccione una sesion",["Sesion 1","Sesion 2","Sesion 3","Sesion 4"])
