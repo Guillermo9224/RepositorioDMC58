b464d068c3bcaab72e75125f9c95fbe86181b0e5
Guillermo9224 <guillermo920624@gmail.com>
2026-05-12 19:56:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,13 @@ st.write("Elaborado por : Guillermo Carrasco")
 
 sesion = st.sidebar.selectbox("Seleccione una sesion",["Sesion 1","Sesion 2","Sesion 3","Sesion 4"])
 
+if sesion == "Sesion 1":
+  st.write("Bienvenido a la sesion 1")
+
+elif sesion =="Sesion 2":
+  st.write("Bienvenido a la sesion 2")
+
+elif sesion =="Sesion 3":
+  st.write("Bienvenido a la sesion 3")
+
+else ("Bienvenido a la sesion 3")
