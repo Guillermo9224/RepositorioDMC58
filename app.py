f316942036a4e551bcb654fda70c6b4af052298c
Guillermo9224 <guillermo920624@gmail.com>
2026-05-12 20:11:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,60 @@
 import streamlit as st
 
-st.title ("Mi primera aplicacion en python")
-
-st.sidebar.title ("Parametros")
-
-st.write("Elaborado por : Guillermo Carrasco")
-st.sidebar.image ("DMC.png")
+st.title("Mi primera aplicación en python")
 
 
-sesion = st.sidebar.selectbox("Seleccione una sesion",["Sesion 1","Sesion 2","Sesion 3","Sesion 4"])
+st.sidebar.title("Parámetros")
 
-if sesion == "Sesion 1":
-  st.write("Bienvenido a la sesion 1")
-  st.image("Python_logo.png")
+ 
 
+st.write("Elaborado por: Guillermo Carrasco")
 
-elif sesion =="Sesion 2":
-  st.write("Bienvenido a la sesion 2")
-  Precio=st.number_input("Ingrese el precio del producto",min_value=0,max_value=5000,value=1200)
-  descuento=st.number_input("Ingrese el descuento del producto del 0 al 100% ", min_value = 0 , max_value = 100)
+ 
 
-precio_final_producto= precio - (precio*descuento)
-st.write("El precio final del producto es: ",precio_fninal_producto)
+st.sidebar.image("DMC.png")
 
-elif sesion =="Sesion 3":
-  st.write("Bienvenido a la sesion 3")
+ 
 
-else :
-  st.write("Bienvenido a la sesion 4")
+sesion = st.sidebar.selectbox("Seleccione una sesión", ["Sesión 1","Sesión 2","Sesión 3","Sesión 4"] )
+
+ 
+
+if sesion == "Sesión 1":
+
+  st.write("Bienvenido la sesión 1")
+
+  st.image("Python_logo.png" )
+
+ 
+
+elif sesion == "Sesión 2":
+
+  st.write("Bienvenido la sesión 2")
+
+ 
+
+  precio = st.number_input("Ingrese el precio del producto", min_value = 0 , max_value = 5000 , value = 1200)
+
+  descuento = st.number_input("Ingrese el descuento del producto del 0 al 100% ", min_value = 0 , max_value = 100 )
+
+ 
+
+  precio_final_producto = precio - (precio*(descuento/100))
+
+ 
+
+  st.write("El precio final del producto es: ", precio_final_producto  )
+
+ 
+elif sesion == "Sesión 3":
+
+  st.write("Bienvenido la sesión 3")
+
+ 
+
+else:
+
+  st.write("Bienvenido la sesión 4")
 
 
 
