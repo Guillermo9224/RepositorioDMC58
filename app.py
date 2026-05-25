7bdf62486b08292f41b869cbcde546ef6a91cc40
Guillermo9224 <guillermo920624@gmail.com>
2026-05-24 23:34:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -108,29 +108,6 @@ else:
 
  
 
-elif Opcion == "Sesión 2":
-
-  st.write("Bienvenido la sesión 2")
-
- 
-
-  precio = st.number_input("Ingrese el precio del producto", min_value = 0 , max_value = 5000 , value = 1200)
-
-  descuento = st.number_input("Ingrese el descuento del producto del 0 al 100% ", min_value = 0 , max_value = 100 )
-
- 
-
-  precio_final_producto = precio - (precio*(descuento/100))
-
- 
-
-  st.write("El precio final del producto es: ", precio_final_producto  )
-
- 
-elif Opcion == "Sesión 3":
-
-  st.write("Bienvenido la sesión 3")
-
  
 
 else:
